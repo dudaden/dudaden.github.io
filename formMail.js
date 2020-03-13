848891aed185094c4c314586d51da1d3bf08b7e7
dudaden <57559295+dudaden@users.noreply.github.com>
2020-03-13 18:52:20 +0000

    My first version of the form

---
+++ formMail.js
@@ -0,0 +1,53 @@
+$("#sendMail").on("click", function() {
+var q1 = $("like").val();
+var q2 = $("often").val();
+var q3 = $("journey").val();
+var q4 = $("travel").val();
+var q5 = $("notcities").val();
+var q6 = $("likeRussia").val();
+var q7 = $("notstandart").val();
+var age = $("age").val();
+
+if(q1 == "") {
+    $("errorMess").text("Please select one");
+    return false;
+} else if(q2 == "") {
+    $("errorMess").text("Please select one");
+    return false;
+} else if(q3 == "") {
+    $("errorMess").text("Please select one");
+    return false;
+} else if(q4 == "") {
+    $("errorMess").text("Please select one");
+    return false;
+} else if(q5 == "") {
+    $("errorMess").text("Please select one");
+    return false;
+} else if(q6 == "") {
+    $("errorMess").text("Please select one");
+    return false;
+} else if(q7 == "") {
+    $("errorMess").text("Please select one");
+    return false;
+} else if(age =="" || age == " ") {
+    $("errorMess").text("Please select one");
+    return false;   }
+    $("errorMess").text("");
+}
+$.ajax({
+    url: 'ajax/mail.php',
+    type: 'POST',
+    cache: false,
+    date: { 'q1': like,  'q2' : often,  'q3' : journey,  'q4' : travel, 'q5' : notcities, 'q6' : likeRussia,  'q7' : notstandart, 'age' : age, },
+    dataType: 'html',
+    beforeSend: function() {
+        $("#sendMail").prop("disabled", true);
+    },
+    success: function(data) {
+        if(!data)
+        alert("Message not send")
+        else
+        $("#mailForm").trigger("reset");
+        $("#sendMail").prop("disabled", false);
+    }
+});
---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+$q1 = $_POST['like']
+$q2 = $_POST['often']
+$q3 = $_POST['journey']
+$q4 = $_POST['travel']
+$q5 = $_POST['notcities']
+$q6 = $_POST['likeRussia']
+$q7 = $_POST['notstandart']
+$age = $_POST['age']
+
+$subject = "=?utf-8?B?".base64_encode("Message for Questionaries")."?=";
+$headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
+
+$success = mail("dzankydepils@gmail.com", $subject, $message, $headers);
+echo $success;
+?>
