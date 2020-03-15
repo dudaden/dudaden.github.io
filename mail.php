4797e48e8a4ac1f917044326252b6a53a92256a8
dudaden <57559295+dudaden@users.noreply.github.com>
2020-03-15 19:15:29 +0000

    commit

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 $q1 = $_POST['like'];
 $q2 = $_POST['often'];
-$q3 = $_POST['journey']
-$q4 = $_POST['travel']
-$q5 = $_POST['notcities']
-$q6 = $_POST['likeRussia']
-$q7 = $_POST['notstandart']
-$age = $_POST['age']
+$q3 = $_POST['journey'];
+$q4 = $_POST['travel'];
+$q5 = $_POST['notcities'];
+$q6 = $_POST['likeRussia'];
+$q7 = $_POST['notstandart'];
+$age = $_POST['age'];
 
 $subject = "=?utf-8?B?".base64_encode("Message for Questionaries")."?=";
 $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
