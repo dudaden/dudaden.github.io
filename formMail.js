51ad1c119112adab52739606cbc8db632fb9745f
dudaden <57559295+dudaden@users.noreply.github.com>
2020-03-21 17:34:46 +0000

    commit

--- formMail.js
+++ formMail.js
@@ -1,13 +1,13 @@
 $("#sendMail").on("click", function() {
     
-    var like = document.querySelector('input[name="q1"]:checked').value;
-    var often = document.querySelector('input[name="q2"]:checked').value;
-    var journey = document.querySelector('input[name="q3"]:checked').value;
-    var travel = document.querySelector('input[name="q4"]:checked').value;
-    var notcities = document.querySelector('input[name="q5"]:checked').value;
-    var likeRussia = document.querySelector('input[name="q6"]:checked').value;
-    var notstandart = document.querySelector('input[name="q7"]:checked').value;
-    var age = document.querySelector('input[name="age"]:checked').value;
+    var like = document.querySelector('input[name="q1"]:checked');
+    var often = document.querySelector('input[name="q2"]:checked');
+    var journey = document.querySelector('input[name="q3"]:checked');
+    var travel = document.querySelector('input[name="q4"]:checked');
+    var notcities = document.querySelector('input[name="q5"]:checked');
+    var likeRussia = document.querySelector('input[name="q6"]:checked');
+    var notstandart = document.querySelector('input[name="q7"]:checked');
+    var age = document.querySelector('input[name="age"]');
 
     console.log(like)
     console.log(often)
@@ -18,36 +18,45 @@ $("#sendMail").on("click", function() {
     console.log(notstandart)
     console.log(age)
 
-    if(like == undefined) {
-        $("errorMess").text("Please select one");
+    if(like === null) {
+        $("#errorMess").text("Please select one");
         return false;
-    } else if(often == undefined) {
-        $("errorMess").text("Please select one");
+    } else if(often === null) {
+        $("#errorMess").text("Please select one");
         return false;
-    } else if(journey == undefined) {
-        $("errorMess").text("Please select one");
+    } else if(journey === null) {
+        $("#errorMess").text("Please select one");
         return false;
-    } else if(travel == undefined) {
-        $("errorMess").text("Please select one");
+    } else if(travel === null) {
+        $("#errorMess").text("Please select one");
         return false;
-    } else if(notcities == undefined) {
-        $("errorMess").text("Please select one");
+    } else if(notcities === null) {
+        $("#errorMess").text("Please select one");
         return false;
-    } else if(likeRussia == undefined) {
-        $("errorMess").text("Please select one");
+    } else if(likeRussia === null) {
+        $("#errorMess").text("Please select one");
         return false;
-    } else if(notstandart == undefined) {
-        $("errorMess").text("Please select one");
+    } else if(notstandart === null) {
+        $("#errorMess").text("Please select one");
         return false;
-    } else if(age =="" || age == " ") {
-        $("errorMess").text("Please select one");
+    } else if(age === null || age.value === "") {
+        $("#errorMess").text("Please select one");
         return false;   
     }
     $.ajax({
         url: 'mail.php',
         type: 'POST',
         cache: false,
-        data: { 'q1': like,  'q2' : often, 'q3' : journey,  'q4' : travel, 'q5' : notcities, 'q6' : likeRussia,  'q7' : notstandart, 'age' : age, },
+        data: { 
+            'q1': like.value,
+            'q2' : often.value,
+            'q3' : journey.value,
+            'q4' : travel.value,
+            'q5' : notcities.value,
+            'q6' : likeRussia.value, 
+            'q7' : notstandart.value, 
+            'age' : age.value, 
+        },
         beforeSend: function() {
             $("#sendMail").prop("disabled", true);
         },
